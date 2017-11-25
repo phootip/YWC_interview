785c9ca3a1c5625b24b3bf1b6324e0c0809e318b
DESKTOP-LVN60EJ\computer <top10_tip@hotmail.com>
2017-11-26 02:01:04 +0700

    available in offline, sortable, animation added, logo added, autoSuggestion added together with search by name

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.css'; 
+import Autosuggest from 'react-autosuggest';
+import myjson from './ywc15.json';
+// import './buttons.css';
+// for boucing in animation
+var time = 0;
+var data = [];
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
+function escapeRegexCharacters(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+function getSuggestions(value) {
+  const escapedValue = escapeRegexCharacters(value.trim());
+  
+  if (escapedValue === '') {
+    return [];
+  }
+
+  const regex = new RegExp('^' + escapedValue, 'i');
+
+  return data.filter(i => regex.test(i.firstName));
+}
+
 
 class Table extends React.Component {
   constructor(){
     super();
     this.state = { 
-      name: 'loading',
-      data: [
-        // {firstName:'load',interviewRef:'ding'},
-        // {firstName:'load',interviewRef:'ding'},
-        // {firstName:'load',interviewRef:'ding'},
-      ]
-    }
+      // data: myjson,
+      data:[],
+      suggestion: [],
+      active : 'CT',
+      sortOrder : 0,
+      sortBy:"interviewRef"
+    };
+    this.editTable = this.editTable.bind(this);
 
     // Get API
     fetch('https://ywc15.ywc.in.th/api/interview')
@@ -20,51 +43,74 @@ class Table extends React.Component {
       return response.json()
     }).then((json) => {
       this.setState({data:json});
+      // update outside would be too fast
+      data = this.state.data;
     }).catch((ex) =>  {
       console.log('parsing failed', ex)
     })
-  }
 
-  count(){
-    var c = this.state.data.length;
-    console.log(c);
-    if(c > 0){
-      var d = Object.keys(this.state.data[0]);
-      console.log(d);
-      console.log(d.length);
-    }
-    return c;
+    // // update data for autoSuggestion
+    // data = this.state.data;
   }
 
   createRow(){
-    var data = this.state.data;
-    // var result =  [];
-    // for(var i=0;i<data.length;i++){
-
-    // }
-    var listRow = data.map((i) => 
-      <tr>
-        <td> {i.firstName} </td>
-        <td> {i.lastName} </td>
-        <td> {i.major} </td>
-        <td> {i.interviewRef} </td>
-      </tr>
-  );
-    return listRow;
+    time = 0;
+    var data = [];
+    if(this.state.active !== '')data = this.state.data;
+    else data = this.state.suggestion;
+    data.sort((a,b)=>{
+      return (a[this.state.sortBy] < b[this.state.sortBy]? -1:1)}
+    );
+    if(this.state.sortOrder === 1) data.reverse();
+    if(this.state.active !== ''){
+      return data
+      .filter((i) => i.interviewRef.slice(0,2) === this.state.active)
+      .map((i) => {
+        return <Row value={i} key={i.interviewRef} wait={1000}/>
+      });
+    }else{
+      return data.map((i) => {
+        return <Row value={i} key={i.interviewRef} wait={1000}/>
+      });
+    }
+    
   }
   
+  editTable(code,suggestion){
+    this.setState({active:code});
+    this.setState({suggestion:suggestion});
+
+  }
+
+  iconRender(sortBy){
+    if(this.state.sortBy === sortBy)
+      if(this.state.sortOrder) return "fa-sort-asc";
+      else return "fa-sort-desc";
+    else return "fa-sort";
+  }
+
+  iconClick(sortBy){
+    if(this.state.sortBy === sortBy){
+      if(this.state.sortOrder === 1) this.setState({sortOrder:0})
+      else this.setState({sortOrder:1})
+    }
+    else{
+      this.setState({sortBy:sortBy,sortOrder:0})
+    }
+  }
+
   render(){
     return(
       <div>
-        <h1> This is a table we will be using</h1>
-        {this.count()}
-        <table className="table">
+        <App editTable={this.editTable}/>
+        <ButtonBlock editTable={this.editTable}/>
+        <table className="table table-striped">
           <thead>
             <tr>
-              <th> Firstname </th>
-              <th> Lastname </th>
-              <th> Major </th>
-              <th> interviewRef </th>
+              <th> Firstname <Icon icon={this.iconRender('firstName')} onClick={()=>this.iconClick('firstName')}/> </th>
+              <th> Lastname <Icon icon={this.iconRender('lastName')} onClick={()=>this.iconClick('lastName')}/> </th>
+              <th> Major <Icon icon={this.iconRender('major')} onClick={()=>this.iconClick('major')}/> </th>
+              <th> interviewRef <Icon icon={this.iconRender('interviewRef')} onClick={()=>this.iconClick('interviewRef')}/> </th>
             </tr>
           </thead>
           <tbody>
@@ -77,22 +123,152 @@ class Table extends React.Component {
     )
   }
 }
-
 class Row extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      status:"hidden",
+      class:"bounceInUp animated "
+    }
+
+    setTimeout(()=>{
+      this.setState({status:""})
+    },time)
+    time += 100;
+  }
   render(){
-    console.log(this.props.value);
     return(
-      <tr>
-        <td> {this.props.value["firstName"]} </td>
-        <td> 2 </td>
-        <td> 3 </td>
-        <td> 4 </td>
+      <tr className={this.state.class+this.state.status}>
+        <td> {this.props.value.firstName} </td>
+        <td> {this.props.value.lastName} </td>
+        <td> {this.props.value.major} </td>
+        <td> {this.props.value.interviewRef} </td>
       </tr>
+    );
+  }
+}
+class ButtonBlock extends React.Component{
+
+  buttonClick(code){
+    this.setState({active:code});
+    this.props.onClick(code);
+  }
+
+  renderButton(name,code){
+    return(
+      <Button 
+        value={name}
+        onClick={() => this.props.editTable(code)}
+      />
+    )
+  }
+
+  render(){
+    return(
+      <div>
+        {this.renderButton("content","CT")}
+        {this.renderButton("design","DS")}
+        {this.renderButton("marketing","MK")}
+        {this.renderButton("programing","PG")}
+      </div>
+    )
+  }
+}
+class Button extends React.Component{
+  render(){
+    return(
+        <button className="button button--wayra button--border-thick button--text-upper button--size-s"
+        onClick={()=> this.props.onClick()}>
+          {this.props.value}
+        </button>
+    )
+  }
+}
+class Icon extends React.Component{
+  render(){
+    const iconStyle = {float:'right',marginRight:20}
+    const fa = "fa ";
+    return(
+      <i className={fa+this.props.icon} aria-hidden="true" style={iconStyle} onClick={()=>this.props.onClick()}></i>
+    )
+  }
+}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: '',
+      suggestions: [],
+      a:0
+    };    
+  }
+
+  onChange = (event, { newValue, method }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+  
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  renderSuggestion(suggestion) {
+    return (
+      <span>{suggestion.firstName +" "+ suggestion.lastName}</span>
+    );
+  }
+
+  getSuggestionValue(suggestion) {
+    return suggestion.firstName +" "+ suggestion.lastName;
+  }
+
+  onSuggestionSelected = (event, { suggestion, suggestionValue, suggestionIndex, sectionIndex, method }) => {
+    this.props.editTable("",[suggestion]);
+    this.setState({a:1});
+  }
+
+  onSubmit = (e) => {
+    if(typeof e !== "undefined") e.preventDefault();
+    if(this.state.a===0 && this.state.value!=="") this.props.editTable("",this.state.suggestions);
+    else this.setState({a:0})
+  }
+
+  render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: "Search with name",
+      value,
+      onChange: this.onChange
+    };
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <div className="input-group">
+        <Autosuggest 
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+          onSuggestionSelected={this.onSuggestionSelected}
+          getSuggestionValue={this.getSuggestionValue}
+          renderSuggestion={this.renderSuggestion}
+          inputProps={inputProps} />
+          <span className="input-group-addon"><i className="fa fa-search fa-fw fa-2x"></i></span>
+        </div>
+      </form>
     );
   }
 }
 
 ReactDOM.render(
   <Table />,
-  document.getElementById('table')
+  document.getElementById('root')
 );
